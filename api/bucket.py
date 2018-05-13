ee0033b80afbb9daa4932d2f57fb01002e2c685d
Emmanuel Kakaire <kakaemma1@gmail.com>
2018-05-13 21:20:46 +0100

    Added registration endpoint

--- api/bucket.py
+++ api/bucket.py
@@ -1,5 +1,6 @@
 from flask import jsonify, request, json, render_template
 from api import create_app
+from classes.auth import Authenticate
 
 app = create_app('TestingEnv')
 
@@ -10,16 +11,39 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/auth/register')
+@app.route('/auth/register', methods=['POST'])
 def register():
-    """ """
-    return ''
+    """ Register a user with this endpoint """
 
+    request.get_json(force=True)
+    try:
+        email = request.json['email']
+        name = request.json['name']
+        password = request.json['password']
+        response = Authenticate.register(name, email, password)
+        response = operation_successful(response)
+
+        return response
+
+    except KeyError:
+        invalid_keys()
 
 @app.route('/auth/login')
 def login():
     pass
 
+
+def invalid_keys():
+    response = jsonify({'Error': 'Invalid keys'})
+    response.status_code = 400
+    return  response
+
+def operation_successful(response):
+    if response.status_code == 201:
+        data = json.loads(response.data.decode())
+        response = jsonify(data)
+        response.status_code =201
+    return response
 
 
 
