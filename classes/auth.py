a97c7b54897d765a1ba5640531b7003db965f3aa
Emmanuel Kakaire <kakaemma1@gmail.com>
2018-05-17 00:13:20 +0100

    Added methods for reset password

--- classes/auth.py
+++ classes/auth.py
@@ -33,6 +33,11 @@ class Authenticate(object):
             return response
 
         check_user = UserModal.check_user_email(email)
+        if email == check_user:
+            response = jsonify({'Conflict': 'Email already exists'})
+            response.status_code = 409
+            return response
+
         if email != check_user:
             new_user = UserModal(name, email, password)
             new_user.add_user()
@@ -40,9 +45,7 @@ class Authenticate(object):
             response.status_code = 201
             return response
 
-        response = jsonify({'Conflict': 'Email already exists'})
-        response.status_code = 409
-        return response
+
 
     @staticmethod
     def login(email, password):
@@ -75,5 +78,44 @@ class Authenticate(object):
         response.status_code = 200
         return response
 
+    @staticmethod
+    def reset_password(email, old_pass, new_pass):
+        if not email or not new_pass or not old_pass:
+            response = jsonify({
+                'Error': 'Missing email or password'
+            })
+            response.status_code = 422
+            return response
 
+        check_for_user_by_email = UserModal.check_user_return_pass(email)
 
+        if not check_for_user_by_email or not \
+                        old_pass == check_for_user_by_email:
+            response = jsonify({
+                'Error': 'Email and password do not exist'
+            })
+            response.status_code = 403
+            return response
+
+        check_for_password = UserModal.check_user(email, old_pass)
+        if not check_for_password:
+            response = jsonify({'Error': 'Old password is wrong'})
+            response.status_code = 401
+            return response
+
+        if old_pass == new_pass:
+            response = jsonify({
+                'Error': 'Old password and new password are the same'
+            })
+            response.status_code = 401
+            return response
+
+        reset_password = UserModal.reset_user_pass(email, new_pass)
+        if not reset_password:
+            response = jsonify({'Error': 'Password change failed'})
+            response.status_code = 401
+            return  response
+
+        response = jsonify({'Success': 'Password reset successfully'})
+        response.status_code = 200
+        return response
