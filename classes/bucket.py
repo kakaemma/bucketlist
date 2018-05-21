19c8432c559e0e825f066f0e60f71effcb9b80bc
Emmanuel Kakaire <kakaemma1@gmail.com>
2018-05-21 13:39:08 +0100

    Added method to handle creating a bucket

---
+++ classes/bucket.py
@@ -0,0 +1,21 @@
+from flask import jsonify
+from models.bucket_model import BucketModal
+
+class Bucket(object):
+
+    @classmethod
+    def add_bucket(cls, name, desc, user_id):
+        if not name or not desc:
+            response = jsonify({'Error': 'Missing details'})
+            response.status_code = 400
+            return response
+
+        add_bucket = BucketModal(name, desc)
+        response = jsonify({
+            'msg': 'Bucket " ' + name + ' " added',
+            'id': user_id
+        })
+        response.status_code = 200
+        return response
+
+
