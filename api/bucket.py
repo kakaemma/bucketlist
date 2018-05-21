f08a9bf48889a7fa0cf7bc19b0939d75a33662a9
Emmanuel Kakaire <kakaemma1@gmail.com>
2018-05-21 20:14:00 +0100

    Modified token generation

--- api/bucket.py
+++ api/bucket.py
@@ -106,8 +106,7 @@ def operation_successful(response):
     """
     if response.status_code == 201:
         data = json.loads(response.data.decode())
-        data['token'] = encode_auth_token(data['id']).decode('utf-8')
-        # data['token'].decode()
+        # data['token'] = encode_auth_token(data['id']).decode('utf-8')
         response = jsonify(data)
         response.status_code = 201
         return response
