f3609266da1dbb09e7eb7e2e358e501bc5273868
Emmanuel Kakaire <kakaemma1@gmail.com>
2018-05-27 22:33:18 +0100

    Added comments

--- classes/bucket.py
+++ classes/bucket.py
@@ -4,7 +4,7 @@ from models.bucket_model import BucketModal
 class Bucket(object):
 
     @classmethod
-    def add_bucket(cls, name, desc, user_id):
+    def add_bucket(cls, name, desc):
         if not name or not desc:
             response = jsonify({'Error': 'Missing details'})
             response.status_code = 400
@@ -14,7 +14,7 @@ class Bucket(object):
         add_bucket = BucketModal(name, desc)
         response = jsonify({
             'message': 'Bucket " ' + name + ' " added',
-            'id': user_id
+            'id': 'user_id'
         })
         response.status_code = 201
         return response
