f69fe4350d6833f5a422c8feee5e871ac6618009
Emmanuel Kakaire <kakaemma1@gmail.com>
2018-06-05 08:32:33 +0100

    Added Doc strings to tests

--- tests/test_bucket.py
+++ tests/test_bucket.py
@@ -36,11 +36,13 @@ class TestBucket(unittest.TestCase):
         response = self.client.post('/auth/register', data=self.user)
 
     def test_get_all_buckets_on_empty_bucket_list(self):
+        """ Test get all buckets when bucket list is empty"""
         response = self.client.get('/buckets')
         self.assertEquals(response.status_code, 404)
         self.assertIn('No buckets available', response.data.decode())
 
     def test_get_single_bucket_with_no_id(self):
+        """Return bucket Id missing"""
         self.tearDown()
         response = self.client.get('/buckets/0')
         self.assertEquals(response.status_code, 400)
@@ -48,6 +50,7 @@ class TestBucket(unittest.TestCase):
         self.tearDown()
 
     def test_get_single_bucket_with_no_bucket_list(self):
+        """ Should return no bucket list added"""
         self.tearDown()
         response = self.client.get('/buckets/1')
         self.assertEquals(response.status_code, 400)
@@ -55,19 +58,21 @@ class TestBucket(unittest.TestCase):
 
 
     def test_get_single_bucket_that_does_not_exist(self):
-        self.tearDown()
+        """ Should return bucket does not exist"""
         self.client.post('/buckets', data=self.bucket2)
         response = self.client.get('/buckets/45')
         self.assertEquals(response.status_code, 404)
         self.assertIn('Bucket does not exist', response.data.decode())
 
     def test_get_single_bucket_successfully(self):
+        """ Return message with bucket"""
         self.client.post('/buckets', data=self.bucket2)
         response = self.client.get('/buckets/1')
         self.assertEquals(response.status_code, 200)
         self.assertIn('Bucket', response.data.decode())
 
     def test_add_bucket_without_name(self):
+        """ Should return missing details"""
         bucket = json.dumps({
             'name': '',
             'desc': 'rally wins'
@@ -77,13 +82,14 @@ class TestBucket(unittest.TestCase):
         self.assertIn('Missing details', response.data.decode())
 
     def test_add_bucket_successfully(self):
+        """Return message with bucket on success"""
         response = self.client.post('/buckets', data=self.bucket)
         self.assertEquals(response.status_code, 201)
         self.assertIn('Bucket', response.data.decode())
 
 
     def test_add_bucket_with_existing_bucket(self):
-
+        """Return bucket already exists"""
         self.client.post('/buckets', data=self.bucket_conf)
         response = self.client.post('/buckets', data=self.bucket_conf)
         self.assertEquals(response.status_code, 409)
